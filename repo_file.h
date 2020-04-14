d547555907fb02ed6043242cd833efc12d6b3149
robyrobt <pomohaci1roby@gmail.com>
2020-04-14 19:08:21 +0300

    da

--- repo_file.h
+++ repo_file.h
@@ -8,11 +8,15 @@ class RepoFile : public Repo<T> {
 private:
 	char* file_name;
 public:
-	RepoFile();
+	RepoFile<T>();
 	RepoFile(const char*);
 	~RepoFile();
 	void loadFromFile(const char*);
 	void saveToFile();
 };
+
+template<class T>
+RepoFile<T>::RepoFile<T>(){}
+
 
 #endif // !REPOFILE_H
